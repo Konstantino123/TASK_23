be9bdc6eb4dbca998d417d9caafe1bd325faa547
Konstantino123 <kt79@bk.ru>
2022-10-27 13:18:07 +0300

    New program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Принимает число N и выдает квадраты чисел от 1 до N
+int GetNumber(string message)
+{
+    int result;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(),out int number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Введите не число или число равное нулю. Повторите ввод \n");
+            }
+    }
+    return result;
+}
+void Square(int n)
+{
+for(int i=1; i <=n; i++)
+{
+    Console.Write($"{i*i},");
+}
+}
+int number = GetNumber("Введите число N");
+Square(number);
